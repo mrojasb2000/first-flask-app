8ebe865abe663696396300c2918615e13ea91fd9
mauricio rojas <mrojasb2000@gmail.com>
2023-08-14 19:39:59 -0400

    chore: add route with variable

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route('/api')
 def my_microservice():
-    return jsonify({'Hello': 'World!'})
+    response = jsonify({'Hello': 'World!'})
+    return response
+
+
+@app.route('/api/person/<id>')
+def person(id):
+    response = jsonify({'Hello': id})
+    return response
 
 
 if __name__ == '__main__':
+    print(app.url_map)
     app.run()
