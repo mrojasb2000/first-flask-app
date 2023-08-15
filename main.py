cd324a447a5fa36ce59df99c2ddbda4ad587012f
mauricio rojas <mrojasb2000@gmail.com>
2023-08-14 21:36:19 -0400

    feat: add subclass converter

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from werkzeug.routing import BaseConverter, ValidationError
+
+_USERS = {'1': 'Tarek', '2': 'Freya'}
+_IDS = {val: id for id, val in _USERS.items()}
+
+
+class RegisteredUser(BaseConverter):
+    def to_python(self, value):
+        if value in _USERS:
+            return _USERS[value]
+        raise ValidationError()
+
+    def to_url(self, value):
+        return _IDS[value]
+
 
 app = Flask(__name__)
+app.url_map.converters['registered'] = RegisteredUser
 
 
 @app.route('/api')
@@ -9,9 +25,15 @@ def my_microservice():
     return response
 
 
-@app.route('/api/person/<id>')
-def person(id):
+@app.route('/api/custom/<id>')
+def custom(id):
     response = jsonify({'Hello': id})
+    return response
+
+
+@app.route('/api/person/<registered:name>')
+def person(name):
+    response = jsonify({'Hello hey': name})
     return response
 
 
